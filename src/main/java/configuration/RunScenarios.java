83a3d4357fffe2173c40b7b8e59d33b4685e5bd3
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-04-07 19:40:47 +0200

    updating class RunScenarios.java

--- src/main/java/configuration/RunScenarios.java
+++ src/main/java/configuration/RunScenarios.java
@@ -10,14 +10,12 @@ import org.junit.runner.RunWith;
     @RunWith(Cucumber.class)
     @CucumberOptions(
             plugin = {"pretty", "summary", "json:target/report/cucumber.json", "junit:target/report/cucumber.xml"},
-            features = {"src/test/java/com/mastercard/dis/mids/bdd/features"},
-            glue = {"com.mastercard.dis.mids.bdd.steps"})
+            features = {"src/main/java/resources/features"},
+            glue = {"steps"})
     public class RunScenarios {
         @BeforeClass
         public static void initAll() {
             Log.info("STARTING Cucumber Session");
-
-
             Log.info("Website url: " + AppContext.Url);
         }
 
