8cda40dfd6db10177093d86bc74be7122951edeb
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-04-08 14:17:59 +0200

    added new SignInPageActions.java

---
+++ src/main/java/pageActions/SignInPageActions.java
@@ -0,0 +1,12 @@
+package pageActions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import pageObjects.SignInPage;
+
+public class SignInPageActions {
+    @Autowired
+    public SignInPage signInPage;
+
+    public void loggedInExistingUser() {
+    }
+}
