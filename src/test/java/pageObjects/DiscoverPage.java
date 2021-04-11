c824715f35f3c8a661816a283a7754f5615e5678
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-04-11 09:36:45 +0200

    added new elements to DiscoverPage.java

--- src/test/java/pageObjects/DiscoverPage.java
+++ src/test/java/pageObjects/DiscoverPage.java
@@ -9,6 +9,9 @@ public class DiscoverPage {
     public static By divEasyLevel = By.cssSelector("[data-test-id=\"difficulty_easy\"]");
     public static By divModerateLevel = By.cssSelector("[data-test-id=\"difficulty_moderate\"]");
     public static By divTourDifficultyTitle = By.cssSelector("[data-test-id=\"tour_difficulty\"]");
+    public static By divSliderHandleLeft = By.cssSelector("div.c-range-slider__handle.c-range-slider__handle-0");
+    public static By divSliderHandleRight = By.cssSelector("div.c-range-slider__handle.c-range-slider__handle-1");
+    public static By divSliderRange = By.cssSelector("div.c-range-slider__bar.c-range-slider__bar-2");
     public static By linkRestFilter = By.xpath("//*[contains(text(), 'Reset Filter')]");
     public static By inputSearch = By.cssSelector("[data-test-id=\"location_search_input\"]");
     public static By spanToursAround = By.cssSelector("[data-test-id=\"tours_around\"]");
