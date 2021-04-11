b28dc12514cfb994dc5eeeedb4ee9584b41f1f94
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-04-11 09:37:51 +0200

    added methods to support slider move on DiscoverPageActions.java

--- src/test/java/pageActions/DiscoverPageActions.java
+++ src/test/java/pageActions/DiscoverPageActions.java
@@ -1,6 +1,7 @@
 package pageActions;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import pageObjects.DiscoverPage;
 
@@ -8,11 +9,26 @@ import static steps.ScenarioHooks.browserActions;
 
 public class DiscoverPageActions {
 
-    public void searchFor(String cityToFind) {
-        browserActions.click(DiscoverPage.inputSearch);
-        browserActions.click(DiscoverPage.inputSearch);
-        browserActions.sendKeys(DiscoverPage.inputSearch, cityToFind);
-        browserActions.sendKeys(DiscoverPage.inputSearch, Keys.ENTER);
+    private static int slider30MinutesResolution = 16 ;
+
+    public void AssertThatFirstTourHas(String difficultyLevel) {
+        Assert.assertEquals(difficultyLevel, browserActions.getText(DiscoverPage.divTourDifficultyTitle).toLowerCase());
+    }
+
+    public void AssertThatThereAreToursAround(String city) {
+        browserActions.waitForPageLoad();
+        browserActions.waitElementVisible(DiscoverPage.spanToursAround);
+        String toursAroundText = browserActions.getText(DiscoverPage.spanToursAround);
+
+        Assert.assertTrue(Integer.valueOf(toursAroundText.replaceAll("\\D+","")) > 0);
+        Assert.assertTrue(toursAroundText.contains(city));
+    }
+
+    public void AssertThatUserVisitThePage() {
+        browserActions.waitForPageLoad();
+        browserActions.waitElementExists(DiscoverPage.inputSearch);
+
+        Assert.assertEquals(DiscoverPage.Url, browserActions.getDriver().getCurrentUrl());
     }
 
     public void clickChosenDifficulty(String difficultyLevel) {
@@ -30,23 +46,34 @@ public class DiscoverPageActions {
         browserActions.waitElementVisible(DiscoverPage.linkRestFilter);
     }
 
-    public void AssertThatUserVisitThePage() {
-        browserActions.waitForPageLoad();
-        browserActions.waitElementExists(DiscoverPage.inputSearch);
-
-        Assert.assertEquals(DiscoverPage.Url, browserActions.getDriver().getCurrentUrl());
+    public void moveTourDurationSliderBetween(String leftSliderHandleHours, String rightSliderHandleHours) {
+        browserActions.slideHorizontallyByPixels(
+                DiscoverPage.divSliderHandleLeft,
+                getSlideBy(leftSliderHandleHours, getSliderPixelResolution(DiscoverPage.divSliderRange)));
     }
 
-    public void AssertThatThereAreToursAround(String city) {
-        browserActions.waitForPageLoad();
-        browserActions.waitElementVisible(DiscoverPage.spanToursAround);
-        String toursAroundText = browserActions.getText(DiscoverPage.spanToursAround);
-
-        Assert.assertTrue(Integer.valueOf(toursAroundText.replaceAll("\\D+","")) > 0);
-        Assert.assertTrue(toursAroundText.contains(city));
+    public void searchFor(String cityToFind) {
+        browserActions.click(DiscoverPage.inputSearch);
+        browserActions.click(DiscoverPage.inputSearch);
+        browserActions.sendKeys(DiscoverPage.inputSearch, cityToFind);
+        browserActions.sendKeys(DiscoverPage.inputSearch, Keys.ENTER);
     }
 
-    public void AssertThatFirstTourHas(String difficultyLevel) {
-        Assert.assertEquals(difficultyLevel, browserActions.getText(DiscoverPage.divTourDifficultyTitle).toLowerCase());
+    private int getSlideBy(String leftSliderHandleHours, double sliderPixelResolution) {
+        double slideBy = sliderPixelResolution * (Double.valueOf(leftSliderHandleHours) * 2 );
+        double n = slideBy - Math.floor(slideBy);
+        if(n < 0.7) {
+            slideBy = Math.floor(slideBy);
+        }
+        else {
+            slideBy = Math.ceil(slideBy);
+        }
+        return Integer.valueOf((int) slideBy);
+    }
+
+    private double getSliderPixelResolution(By target) {
+        browserActions.waitElementVisible(target);
+        return Double.valueOf(browserActions.findElement(target).getAttribute("offsetLeft"))
+                / slider30MinutesResolution;
     }
 }
